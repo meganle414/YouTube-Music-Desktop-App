774947d6e43121c35ed213143aa6f23b0be9bf84
Megan L <meganle414@gmail.com>
2024-09-10 18:46:34 -0700

    Fixed issue with closing app while media is playing.

--- index.js
+++ index.js
@@ -2,24 +2,15 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
-const fs = require('fs');
-const logFilePath = path.join(app.getPath('userData'), 'app.log');
-
-function log(message) {
-    fs.appendFileSync(logFilePath, `${new Date().toISOString()} - ${message}\n`);
-}
-
-log('App started');
-
+let mainWindow;
+let store;
 let Store;
+
 (async () => {
     const module = await import('electron-store');
     Store = module.default;
     initializeApp();
 })();
-
-let mainWindow;
-let store;
 
 function createWindow() {
     log('Creating window');
@@ -39,43 +30,27 @@ function createWindow() {
     // Load YouTube Music URL
     mainWindow.loadURL('https://music.youtube.com/');
 
-    mainWindow.webContents.on('did-fail-load', (event, errorCode, errorDescription) => {
-        log(`Failed to load content: ${errorDescription}`);
-    });
-
-    // Open DevTools for debugging
-    // mainWindow.webContents.openDevTools();
-
     // Restore volume settings
     const savedVolume = store.get('volume', 0.5);
     mainWindow.webContents.executeJavaScript(`
         document.querySelector('video').volume = ${savedVolume};
     `);
 
-    mainWindow.on('closed', () => {
-        log('Window close event triggered');
+    mainWindow.on('close', () => {
         mainWindow = null;
     });
-
-    // Ensure the window is visible
-    // mainWindow.once('ready-to-show', () => {
-    //     mainWindow.show();
-    //     mainWindow.focus();
-    //     log('Window should be visible and focused');
-    // });
 }
 
 function initializeApp() {
-    log('App initializing');
-    store = new Store();  // Initialize `Store` after it's imported
-    log('Store initialized');
+    // Initialize `Store` after it's imported
+    store = new Store();
 
-    // app.whenReady().then(createWindow);
     app.whenReady().then(() => {
         log('App is ready');
         createWindow();
       
         app.on('activate', () => {
+          // Open a window if clicked, despite how many are already open
           if (BrowserWindow.getAllWindows().length === 0) {
             createWindow();
           }
@@ -87,16 +62,7 @@ function initializeApp() {
         store.set('volume', volume);
     });
 
-    app.on('before-quit', (event) => {
-        log('App is about to quit');
-    });
-
-    app.on('will-quit', () => {
-        log('App will quit');
-    });    
-
     app.on('window-all-closed', () => {
-        log('All windows are closed');
         if (process.platform !== 'darwin') {
             app.quit();
         }
